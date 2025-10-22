b7bb3fca1cce0e5fafc6817e807fd8cec9f2b3d9
laudiaghea <gelaudia56@gmail.com>
2025-10-22 18:23:20 +1400

    memodifikasi tanpa menyimpan

--- dashboard.php
+++ dashboard.php
@@ -1,9 +1,14 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
+
+    <link rel="stylesheet" href="dashboard.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+
 </head>
 <body>
     <h1>Selamat Datang di Ghenadi Sinar Abadi</h1>
