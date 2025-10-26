4cba99947dd94bba5d66a1b3a695cd6b458e660d
laudiaghea <gelaudia56@gmail.com>
2025-10-27 13:26:07 +1400

    merubah ukuran

--- beranda.php
+++ beranda.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Beranda</title>
 
-    <link rel="stylesheet" href="beranda.css">
+    <link rel="stylesheet" href="beranda.css?v=<?php echo time(); ?>">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
 
 </head>
@@ -28,7 +28,7 @@
 <section id="hero" class="hero">
     <div class="hero-content">
         <h2>Selamat Datang di Sistem Manajemen Toko Bangunan <span>Sinar Abadi</span></h2>
-        <p>MUdahkan pengelolaan tugas dan data toko</p>
+        <p>Mudahkan pengelolaan tugas dan data toko</p>
         <a href="login.php" class="button">Login Sekarang</a>
     </div>
     <div class="hero-img">
@@ -45,22 +45,22 @@
 <section id="layanan" class="layanan">
     <h2>Layanan Kami</h2>
     <div class="cards">
-        <div class="card" style="background-color: #c9d7f3;">
+        <div class="card">
             <h3>👨‍💼👩‍💼</h3>
             <h3>Manajemen Karyawan</h3>
             <p>Tambah, edit, dan pantau data karyawan dengan mudah</p>
         </div>
-        <div class="card" style="background-color: #c9d7f3;">
+        <div class="card">
             <h3>📤</h3>
             <h3>Data Penjualan</h3>
             <p>Input dan pantau penjualan harian</p>
         </div>
-        <div class="card" style="background-color: #c9d7f3;">
+        <div class="card">
             <h3>📦</h3>
             <h3>Stok Barang</h3>
             <p>Cek stok barang yang tersedia ditoko kami</p>
         </div>
-        <div class="card" style="background-color: #c9d7f3;">
+        <div class="card">
             <h3>🚚</h3>
             <h3>Data Pembelian dan Supplier</h3>
             <p>Input dan pantau data pemeblian barang dari supplier</p>
