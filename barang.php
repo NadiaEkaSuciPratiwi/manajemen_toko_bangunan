93b55996869f1564fcf6b5f7a38daeacdd4c455a
laudiaghea <gelaudia56@gmail.com>
2025-10-28 10:57:10 +1400

    merubah file barang

--- barang.php
+++ barang.php
@@ -13,7 +13,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
 
 </head>
-<>
+<body>
     <?php include 'sidebar.php'; ?>
 
     <div class="navbar">
