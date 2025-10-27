d57de3e8f278e179118d051416d3be38210bd66f
laudiaghea <gelaudia56@gmail.com>
2025-10-28 10:14:32 +1400

    menambah file baru

---
+++ sidebar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navigasi</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="sidebar">
+        <img src="logo.jpg" class="logo">
+        <h2>Sinar Abadi</h2> 
+        <ul>
+            <li><a href="dashboard.php">Dashboard</a></li>
+            <li><a href="karyawan.php">Karyawan</a></li>
+            <li><a href="penjualan.php">Penjualan</a></li>
+            <li><a href="pembelian.php">Pembelian</a></li>
+            <li><a href="barang.php">Barang</a></li>
+        </ul>
+    </div>
+    
+</body>
+</html>
