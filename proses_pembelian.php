95cf36ea1996cdbf7ef153202f2166178d198688
NadiaEkaSuciPratiwi <nadiaa13n@gmail.com>
2025-11-04 12:50:36 +0700

    merubah

--- proses_pembelian.php
+++ proses_pembelian.php
@@ -15,14 +15,14 @@ if(!isset($_SESSION['user_id'])){
     
     }
 
-$query = mysqli_query($koneksi, "SELECT pembelian1.id_pembelian, supplier.nama_supplier, barang.nama_barang,
-           pembelian1.jumlah, pembelian1.harga_beli,
-           (pembelian1.jumlah * pembelian1.harga_beli) AS total_harga,
-           pembelian1.tanggal_pembelian
-    FROM pembelian1
-    JOIN supplier ON pembelian1.id_supplier = supplier.id_supplier
-    JOIN barang ON pembelian1.id_barang = barang.id_barang
-    ORDER BY pembelian1.id_pembelian DESC");
+$query = mysqli_query($koneksi, "SELECT pembelian.id_pembelian, supplier.nama_supplier, barang.nama_barang,
+           pembelian.jumlah, pembelian.harga_beli,
+           (pembelian.jumlah * pembelian.harga_beli) AS total_harga,
+           pembelian.tanggal_pembelian
+    FROM pembelian
+    JOIN supplier ON pembelian.id_supplier = supplier.id_supplier
+    JOIN barang ON pembelian.id_barang = barang.id_barang
+    ORDER BY pembelian.id_pembelian DESC");
 
     if(!$query){
         die("Query Error: " . mysqli_error($koneksi));
